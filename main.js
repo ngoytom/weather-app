27e4a29e64f665d1f0a6b205cfb6fa0d0ee3c50e
Tommy Ngoy <tommy.ngoy@outlook.com>
2021-08-19 12:57:53 -0400

    add local date/time + advanced weather layout (bootstrap)

--- main.js
+++ main.js
@@ -1,8 +1,58 @@
 const form = document.querySelector("form")
 const submit = document.querySelector(".submit-location")
+const clock = document.querySelector(".time")
+const date = document.querySelector(".date")
+
+daily = document.querySelector(".daily")
+hourly = document.querySelector(".hourly")
 
 form.addEventListener("submit",getLocation)
 submit.addEventListener("click",getLocation)
+
+//When pages launch, load local time + date
+updateLocalTime = () => {
+
+    const now = new Date();
+    let hour = now.getHours();
+    const min = String(now.getMinutes()).padStart(2,"0");
+    const sec = String(now.getSeconds()).padStart(2,"0");
+    let desc = "AM"
+
+    if (hour > 12){
+        desc = "PM";
+        hour -= 12;
+    }
+    else{
+        desc = "AM";
+    }
+    clock.textContent = `${hour}:${min}:${sec} ${desc}`
+}
+
+setInterval(updateLocalTime, 1000) //Every 1 second
+
+updateDate = () => {
+    const now = new Date();
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+    
+    date.textContent = `${day}/${month}/${year}`
+}
+
+start = () => {
+    updateLocalTime();
+    updateDate();
+}
+
+displayHourly = () => {
+   daily.style.display = "none";
+   hourly.style.display = "block"
+}
+
+displayDaily = () => {
+    daily.style.display = "block";
+    hourly.style.display = "none"
+ }
 
 function getLocation(e){
     e.preventDefault(); //Prevents page from refreshing when submitting text field
@@ -20,9 +70,16 @@ async function getWeatherData(location){
     const weatherData = await response.json();
     console.log(weatherData);
     const newData = processWeatherData(weatherData);
+    updateCityTime(weatherData);
     displayData(newData);
 
 }
+
+//Change Local Time to City Time of city searched ADVANCED WEATHER FUNCTION
+updateCityTime = (weatherData) => {
+    //https://stackoverflow.com/questions/10087819/convert-date-to-another-timezone-in-javascript
+    console.log(new Date(weatherData.dt*1000+(weatherData.timezone*1000))); // plus
+}   
 
 //Gets Current Weather, Feels Like, Humidity, Wind Speed, Country, City Name, Condition. Lon and Lat for second API call
 function processWeatherData(weatherData){
